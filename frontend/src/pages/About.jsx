7981ed8f42b64ef0c7eadeabc60e10b1305a6fb6
MukamaJ-2 <josephmukama67@gmail.com>
2024-09-14 20:56:14 +0300

    Update About.jsx

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -10,7 +10,7 @@ const About = () => {
       <div><img src={assets.about_image} alt="" />
       <div>
         <p>Welcome to prescripto. Your trusted Partner in managing your Health care needd coviently and efficiently. At per, we understand The Challenges indvi duals face when it comes to schedulingdoctor appointments and managing their Health Records</p>
-        <p></p>
+        <p>per is committed to excellence in Healthcare </p>
         <b></b>
         <p></p>
         </div>
