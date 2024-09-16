fa3dd0ef4a7686251666769c7b27b70f379c74ec
MukamaJ-2 <josephmukama67@gmail.com>
2024-09-16 07:45:10 +0300

    Login

--- frontend/src/pages/MyProfile.jsx
+++ frontend/src/pages/MyProfile.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { assets } from '../assets/assets'
 
 const MyProfile = () => {
+
+  const [userData, setUserData] = useState({
+    name: "Joseph Mukama",
+    image:assets.profile_pic,
+    email:'josephmukama67@gmail.com',
+    phone:'+256 706807724',
+    address:{
+      line1:"ucu, Mukono, Uganda",
+      line2:"Kauga,Mukono, Uganda"
+    },
+    gender:'Male',
+    dob:'2002-01-20'
+  })
+
+  const [isEdit, setIsEdit]= useState(true);
+
+
   return (
     <div>
-      
+      <img src={userData.image} alt="" />
+      {
+        isEdit
+        ? <input type="text" value={userData.name} onChange={e => setUserData(prev =>({...prev,name:e.target.value}))}/>
+        : <p>{userData.name}</p>
+      }
     </div>
   )
 }
